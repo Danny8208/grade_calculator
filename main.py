efabf3b3a24f731a79dd8df2c56fa66eab4fd8f6
Danny8208 <danny@rollingkittens.com>
2022-12-03 17:56:02 -0500

    add get_couses api route

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import json
 import sys
 
 from dotenv import dotenv_values
-from flask import Flask, redirect, request
+from flask import Flask, jsonify, redirect, request
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from pymongo import MongoClient
 
@@ -39,7 +40,8 @@ def check_credential(**kwargs):
     cred = kwargs.get("credential")
     json_string = kwargs.get("json")
     credential = (
-        cred if (cred is not None)
+        cred
+        if (cred is not None)
         else Credentials.from_authorized_user_info(info=json.loads(json_string))
     )
     if credential.valid:
@@ -59,6 +61,14 @@ def get_credential():
         return Credentials.from_authorized_user_info(
             info=json.loads(token), scopes=SCOPES
         )
+
+
+def build_service():
+    return build(serviceName="classroom", version="v1", credentials=get_credential())
+
+
+def get_courses():
+    return build_service().courses().list().execute().get("courses", [])
 
 
 @app.route("/login")
@@ -86,6 +96,11 @@ def callback():
     except Exception as error:
         print(error)
         return "encountered unknown error, check logs"
+
+
+@app.route("/api/get_courses")
+def api_get_courses():
+    return jsonify(get_courses())
 
 
 if __name__ == "__main__":
